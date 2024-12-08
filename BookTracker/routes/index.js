337a4f8e34697e8622fc2c8bce1ac8089bc23365
Muskan040 <153301396+Muskan040@users.noreply.github.com>
2024-12-08 10:09:38 -0500

    Delete BookTracker directory

--- BookTracker/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Home page route
-router.get('/', (req, res) => {
-    res.render('home', { user: req.user });
-});
-
-module.exports = router;
